d14454fdb6796423cf5c2678e241e7e3a2620b82
BeardedBiologist <46723997+BeardedBiologist@users.noreply.github.com>
2023-02-27 14:05:30 +0100

    added do loop

--- Program.cs
+++ Program.cs
@@ -1,8 +1,15 @@
-﻿
+﻿bool isValidAge;
 
 do
 {
 
-} while (true);
+    Console.Write("What is your age: ");
+    string? ageText = Console.ReadLine();
 
+    isValidAge = int.TryParse(ageText, out int age);
+    if (isValidAge == false)
+    {
+        Console.WriteLine("THat was an invalid age.");
+    }
+} while (isValidAge == false);
 
