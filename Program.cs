eabfea5c1b050181dad4441bd64819f0bf97cd9b
BeardedBiologist <46723997+BeardedBiologist@users.noreply.github.com>
2023-02-27 14:08:33 +0100

    add printed age, declared variables.

--- Program.cs
+++ Program.cs
@@ -1,15 +1,16 @@
 ﻿bool isValidAge;
-
+int age;
 do
 {
 
     Console.Write("What is your age: ");
     string? ageText = Console.ReadLine();
 
-    isValidAge = int.TryParse(ageText, out int age);
+    isValidAge = int.TryParse(ageText, out age);
     if (isValidAge == false)
     {
         Console.WriteLine("THat was an invalid age.");
     }
 } while (isValidAge == false);
 
+Console.WriteLine($"Your age is {age}");
